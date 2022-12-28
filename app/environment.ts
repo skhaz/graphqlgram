c7017141afa083ce47d3564e9414edfc8d7e2dc9
Rodrigo Delduca <skhaz@users.noreply.github.com>
2022-12-28 09:01:54 -0300

    Better schema parsing

--- app/environment.ts
+++ app/environment.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 
 const schema = z.object({
   PORT: z.coerce.number().positive().default(3000),
-  JWT_SECRET: z.string().min(1),
-  MONGO_DSN: z.string().min(1),
+  JWT_SECRET: z.string().min(6).max(256),
+  MONGO_DSN: z.string().url(),
 })
 
 export const { PORT, JWT_SECRET, MONGO_DSN } = schema.parse(process.env)
